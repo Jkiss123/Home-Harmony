1acf544bc3fe8e51223e19510d1b14f590e747b7
jkiss123 <showparcloudy@gmail.com>
2024-03-30 17:45:56 +0700

    first commit

--- app/src/main/java/com/example/furniturecloudy/CloudyApplication.kt
+++ app/src/main/java/com/example/furniturecloudy/CloudyApplication.kt
@@ -3,6 +3,8 @@ package com.example.furniturecloudy
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 //11/3/2024
+
+// have done
 @HiltAndroidApp
 class CloudyApplication : Application() {
 }
